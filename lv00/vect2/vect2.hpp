53149dd24ec523560742cbde9e0eecd1b16ff03e
vzashev <110407556+99shandhokhan88@users.noreply.github.com>
2026-03-11 17:59:26 +0100

    Refactor vect2 class operator overloads and methods

--- lv00/vect2/vect2.hpp
+++ lv00/vect2/vect2.hpp
@@ -18,45 +18,48 @@ class vect2 {
 
             public:
 
-                    vect2();
-                    ~vect2();
-                    vect2(int a, int b);
-                    vect2(const vect2& other);
-                    vect2& operator=(const vect2& other);
+                    vect2() ;
+                    ~vect2() ;
+                    vect2(int a, int b) ;
+                    vect2(const vect2& other) ;
 
-                    vect2 operator+(const vect2& other) const;
-                    vect2 operator-(const vect2& other) const;
-                    vect2 operator*(int scalar)         const;
+                    vect2& operator=(const vect2& other) ;
 
-                    vect2& operator+=(const vect2& other) ;
-                    vect2& operator-=(const vect2& other) ;
-                    vect2& operator*=(int scalar)         ;
+                    int&       operator[] (int pos)       ;
+                    const int& operator[] (int pos) const ;
 
-                    vect2& operator++(void)  ;
-                    vect2 operator++(int);
-                    vect2& operator--(void)  ;
-                    vect2 operator--(int);
+                    vect2  operator++ (int)  ;
+                    vect2& operator++ (void) ;
+                    vect2  operator-- (int)  ;
+                    vect2& operator-- (void) ;
 
-                    vect2 operator-(void) const;
+                    vect2& operator+= (const vect2& other) ;
+                    vect2& operator-= (const vect2& other) ;
+                    vect2& operator*= (int scalar)         ;
+                    
+                    vect2 operator+ (const vect2& other) const ;
+                    vect2 operator- (const vect2& other) const ;
+                    vect2 operator* (int scalar)         const ;
 
-                    bool operator==(const vect2& other) const;
-                    bool operator!=(const vect2& other) const;
+                    vect2 operator- (void) const ;
 
-                    int&       operator[] (int pos)      ;
-                    const int& operator[] (int pos) const;
+                    bool operator== (const vect2& other) const ;
+                    bool operator!= (const vect2& other) const ;
 
-                    int get_x(void) const;
-                    int get_y(void) const;
+                    int get_x (void) const ;
+                    int get_y (void) const ;
 
 };
 
 
-vect2 operator*(int scalar, const vect2& vector);
+vect2 operator* (int scalar, const vect2& vector) ;
 
 
-std::ostream& operator<<(std::ostream& output, const vect2& vector);
+std::ostream& operator<< (std::ostream& output, const vect2& vector) ;
 
 
 
 
 #endif
+
+// vect2.hpp
