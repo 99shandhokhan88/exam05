af5ddc485b0bde0a3889d73ae67fd80cd347dce4
vzashev <110407556+99shandhokhan88@users.noreply.github.com>
2026-03-19 23:16:03 +0100

    Refactor bigint class interface and member functions

--- lv00/bigint/bigint.hpp
+++ lv00/bigint/bigint.hpp
@@ -1,43 +1,61 @@
 
+
 #ifndef BIGINT_HPP
 #define BIGINT_HPP
+
+
+
 
 #include <string>
 #include <sstream>
 #include <iostream>
 
+
+
 class bigint {
-private:
-    std::string digits;
-    explicit bigint(const std::string& str) : digits(str) {}
-    static void normalize(std::string& digits);
 
-public:
-    bigint();
-    ~bigint();
-    bigint(unsigned long long num);
-    bigint(const bigint& other);
-    bigint& operator=(const bigint& other);
+            private:
 
-    bigint  operator+(const bigint& other) const;
-    bigint& operator+=(const bigint& other);
-    bigint& operator++();
-    bigint  operator++(int);
+                    std::string digits;
 
-    bigint  operator<<(unsigned int num) const;
-    bigint& operator<<=(unsigned int num);
-    bigint& operator>>=(const bigint& other);
+            public:
 
-    bool operator>(const bigint& other)  const;
-    bool operator>=(const bigint& other) const;
-    bool operator<(const bigint& other)  const;
-    bool operator<=(const bigint& other) const;
-    bool operator==(const bigint& other) const;
-    bool operator!=(const bigint& other) const;
+                    bigint() ;
+                    ~bigint() ;
+                    bigint(unsigned long long num) ;
+                    bigint(const bigint& other) ;
 
-    std::string getValue() const;
+                    bigint& operator= (const bigint& other) ;
+
+                    bigint  operator+  (const bigint& other) const ;
+                    bigint& operator+= (const bigint& other)       ;
+                    bigint& operator++ (void)                      ;
+                    bigint  operator++ (int)                       ;
+
+                    bigint  operator<<  (unsigned int num) const ;
+                    bigint& operator<<= (unsigned int num)       ;
+                    bigint& operator>>= (const bigint& other)    ;
+
+                    bool operator>  (const bigint& other) const ;
+                    bool operator>= (const bigint& other) const ;
+                    bool operator<  (const bigint& other) const ;
+                    bool operator<= (const bigint& other) const ;
+                    bool operator== (const bigint& other) const ;
+                    bool operator!= (const bigint& other) const ;
+
+                    static void normalize (std::string& digits) ;
+
+                    std::string get_value(void) const ;
+
 };
 
-std::ostream& operator<<(std::ostream& output, const bigint& print);
+
+std::ostream& operator<< (std::ostream& output, const bigint& print) ;
+
+
+
 
 #endif
+
+// bigint.hpp
+
